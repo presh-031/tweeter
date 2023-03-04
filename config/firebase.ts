3c00148889824cd946fbb3142644a1c053329e53
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-04 23:34:40 +0100

    fix: fixed issues with firebase secrets

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,27 +1,22 @@
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
-
+import { GoogleAuthProvider } from "firebase/auth";
 // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-export const auth = getAuth(app);
+// const analytics = getAnalytics(app);
 export const provider = new GoogleAuthProvider();
--- pages/sign-in.tsx
+++ pages/sign-in.tsx
@@ -3,51 +3,109 @@ import {
   AiOutlineGoogle,
   AiOutlineTwitter,
 } from "react-icons/ai";
+import { auth, provider } from "@/config/firebase";
 
 import { FaFacebookSquare } from "react-icons/fa";
+import { IconContext } from "react-icons";
 import Image from "next/image";
+import { IoMdLock } from "react-icons/io";
+import { MdEmail } from "react-icons/md";
 import logo from "../assets/tweeter.svg";
+import { signInWithPopup } from "firebase/auth";
 
 const signIn = () => {
-  return (
-    <div className="p-4">
-      <header className="mb-8">
-        <Image src={logo} alt="tweeter" />
-      </header>
-      <p className="text-3xl mb-6">Join thousands of users around the world</p>
-      <p className="text-xl mb-12">
-        Welcome to Tweeter, the social network where every tweet counts! Join
-        millions of users worldwide and share your thoughts, opinions, and
-        experiences with the world. From breaking news to hilarious memes,
-        Tweeter has it all. Sign in now and let your voice be heard!
-      </p>
+  // const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
 
-      <form className="flex flex-col gap-3">
-        <input type="text" className="w-full border border-red-800" />
-        <input type="text" className="w-full border border-red-800" />
-        <input
-          type="submit"
-          value="Start tweeting now"
-          className="w-full border border-red-800"
-        />
-      </form>
-      <p className="text-center mt-16">or continue with these social profile</p>
-      <div>
-        <div>
-          <AiOutlineGoogle />
+  //   if (error) {
+  //     return (
+  //       <div>
+  //         <p>Error: {error.message}</p>
+  //       </div>
+  //     );
+  //   }
+  //   if (loading) {
+  //     return <p>Loading...</p>;
+  //   }
+  //   if (user) {
+  //     return (
+  //       <div>
+  //         <p>Signed In User: {user.email}</p>
+  //       </div>
+  //     );
+  //   }
+  const signInWithGoogle = async () => {
+    const result = await signInWithPopup(auth, provider);
+    console.log(result);
+
+    // if (result) {
+    //   navigate("/");
+    // }
+  };
+
+  return (
+    <IconContext.Provider value={{ className: "react-icons-signin" }}>
+      <div className="p-4">
+        <header className="mb-8">
+          <Image src={logo} alt="tweeter" />
+        </header>
+        <p className="text-3xl mb-6">
+          Join thousands of users around the world
+        </p>
+        <p className="text-xl mb-12">
+          Welcome to Tweeter, the social network where every tweet counts! Join
+          millions of users worldwide and share your thoughts, opinions, and
+          experiences with the world. From breaking news to hilarious memes,
+          Tweeter has it all. Sign in now and let your voice be heard!
+        </p>
+
+        <form className="flex flex-col gap-3">
+          <div className="flex items-center border-[1px] pl-4 overflow-hidden border-gray-800 rounded-xl gap-4">
+            <MdEmail />
+            <input
+              type="text"
+              placeholder="Email"
+              className="w-full  py-3 text-2xl outline-none"
+            />
+          </div>
+          <div className="flex items-center border-[1px] pl-4 overflow-hidden border-gray-800 rounded-xl gap-4">
+            <IoMdLock />
+            <input
+              placeholder="Password"
+              type="text"
+              className="w-full  py-3 text-2xl outline-none"
+            />
+          </div>
+          <input
+            type="submit"
+            value="Start tweeting now"
+            className="w-full py-3 rounded-xl text-2xl mt-2 border"
+          />
+        </form>
+
+        <p className="text-center mt-16">
+          or continue with these social profile
+        </p>
+
+        <div className="flex gap-8  justify-center mt-8">
+          <div
+            onClick={signInWithGoogle}
+            className="p-4 border-gray-800 grid items-center border-[1px] rounded-full"
+          >
+            <AiOutlineGoogle />
+          </div>
+          <div className="p-4 border-gray-800 grid items-center border-[1px] rounded-full">
+            <FaFacebookSquare />
+          </div>
+          <div className="p-4 border-gray-800 grid items-center border-[1px] rounded-full">
+            <AiOutlineTwitter />
+          </div>
+          <div className="p-4 border-gray-800 grid items-center border-[1px] rounded-full">
+            <AiFillGithub />
+          </div>
         </div>
-        <div>
-          <FaFacebookSquare />
-        </div>
-        <div>
-          <AiOutlineTwitter />
-        </div>
-        <div>
-          <AiFillGithub />
-        </div>
+        <p className="text-center mt-16">Already a member? Login</p>
       </div>
-      <p>Already a member? Login</p>
-    </div>
+    </IconContext.Provider>
   );
 };
 
