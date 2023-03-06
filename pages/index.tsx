920b324d2d06f85c94b94713c6dcccc039ddba49
presh-031 <akingbulugbe.dev@gmail.com>
2023-03-06 15:26:28 +0100

    now handling new users as login and signin

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { auth } from "@/config/firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 const index = () => {
+  // To get info about current user in users collection, use auth state to get current user id and find the user with the id in the users collection.
   const [user, loading, error] = useAuthState(auth);
   return (
     <div className=" px-[1.90rem]">
