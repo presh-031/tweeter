3ee9ce99c0fc90dde5c4da3da52cc6cfad99c6f5
presh-031 <akingbulugbe.dev@gmail.com>
2023-04-22 11:07:22 +0100

    style: finished styling edit profile page

--- pages/edit.tsx
+++ pages/edit.tsx
@@ -58,72 +58,71 @@ const edit = () => {
 
   return (
     <div className="rounded-2xl px-[1.90rem] pb-[9.615rem]">
-      <div className="p-8 text-2xl outline">
-        <p className="mb-16 text-4xl">Edit Profile</p>
-        <form className="" onSubmit={handleSubmit(onSubmit)}>
-          <div></div>
-
-          <label className="mb-4" htmlFor="name">
-            Name *
+      <div className="py-8 ">
+        <p className="mb-16 text-4xl text-blueish">Edit Profile</p>
+        <form className=" text-2xl " onSubmit={handleSubmit(onSubmit)}>
+          <label className="mb-4 text-[#333333] " htmlFor="name">
+            <span>Name </span>
+            <span>*</span>
           </label>
           <input
             {...register("userName")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             type="text"
             id="userName"
           />
           <p style={{ color: "red" }}>{errors.userName?.message}</p>
 
-          <label className="mb-4" htmlFor="email">
+          <label className="mb-4  text-[#333333]" htmlFor="email">
             Email *
           </label>
           <input
             {...register("email")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             type="text"
             id="email"
           />
           <p style={{ color: "red" }}>{errors.email?.message}</p>
 
-          <label className="mb-4" htmlFor="displayName">
+          <label className="mb-4  text-[#333333]" htmlFor="displayName">
             Display Name *
           </label>
           <input
             {...register("displayName")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             type="text"
             id="displayName"
           />
           <p style={{ color: "red" }}>{errors.displayName?.message}</p>
 
-          <label className="mb-4" htmlFor="profilePictureUrl">
+          <label className="mb-4  text-[#333333]" htmlFor="profilePictureUrl">
             Profile-PicUrl *
           </label>
           <input
             {...register("profilePictureUrl")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             type="text"
             id="profilePictureUrl"
           />
           <p style={{ color: "red" }}>{errors.profilePictureUrl?.message}</p>
 
-          <label className="mb-4" htmlFor="headerImageUrl">
+          <label className="mb-4  text-[#333333]" htmlFor="headerImageUrl">
             Header ImageUrl *
           </label>
           <input
             {...register("headerImageUrl")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             type="text"
             id="headerImageUrl"
           />
           <p style={{ color: "red" }}>{errors.headerImageUrl?.message}</p>
 
-          <label className="mb-4" htmlFor="bio">
+          <label className="mb-4  text-[#333333]" htmlFor="bio">
             Bio
           </label>
           <textarea
             {...register("bio")}
-            className="mb-8 block w-full border border-red-800"
+            className="mb-8 block w-full rounded-xl border-[1px] p-4 outline-none"
             name="bio"
             id="bio"
             cols={30}
@@ -131,10 +130,15 @@ const edit = () => {
           ></textarea>
           <p style={{ color: "red" }}>{errors.bio?.message}</p>
 
-          {/* <label htmlFor="name">Name *</label>
-      <input type="text" id="name" /> */}
-
-          <input type="submit" />
+          <div>
+            <input
+              className="rounded-xl bg-blueish py-4 px-6 text-white"
+              type="submit"
+            />
+            {
+              // loading spinner
+            }
+          </div>
         </form>
       </div>
     </div>
