50ca1796a4f8de80577a0b805a073acf5a98df86
presh-031 <akingbulugbe.dev@gmail.com>
2023-04-25 01:42:38 +0100

    chore: still refactoring tweet

--- pages/tweets/[id].tsx
+++ pages/tweets/[id].tsx
@@ -1,5 +1,8 @@
-import { collection, doc, orderBy, query } from "firebase/firestore";
-import { useCollection, useDocumentData } from "react-firebase-hooks/firestore";
+import { collection, doc, orderBy, query, where } from "firebase/firestore";
+import {
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
 
 import Comment from "@/components/Comment";
 import Tweet from "@/components/Tweet";
@@ -11,29 +14,12 @@ const TweetInfo = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  const [tweet, tweetLoading, tweetError, snapshot] = useDocumentData(
+  const [tweet, tweetLoading, tweetError, tweetSnapshot] = useDocumentData(
     doc(db, "tweets", id),
     {
       snapshotListenOptions: { includeMetadataChanges: true },
     }
   );
-  // useEffect(() => {
-  //   if (id) {
-  //     const getTweet = async () => {
-  //       const tweetRef = doc(db, "tweets", id);
-
-  //       try {
-  //         const tweetSnap = await getDoc(tweetRef);
-  //         const tweetDoc = tweetSnap.data();
-
-  //         setTweet(tweetDoc);
-  //       } catch (err) {
-  //         console.error(err);
-  //       }
-  //     };
-  //     getTweet();
-  //   }
-  // }, []);
 
   // Logic to get tweet comments
   // Get all tweet's comments with the id
@@ -41,54 +27,56 @@ const TweetInfo = () => {
 
   const commentsRef = collection(db, "comments");
 
-  // Create a query object that orders the documents by the "timestamp" field
-  const commentsQuery = query(commentsRef, orderBy("timestamp", "desc"));
+  // Query object that orders the documents by the "timestamp" field
+  const commentsQuery = query(
+    commentsRef,
+    where("tweetId", "==", id),
+    orderBy("timestamp", "desc")
+  );
 
-  const [commentsListSnapshot, loading, error] = useCollection(commentsQuery, {
-    snapshotListenOptions: { includeMetadataChanges: true },
-  });
-  const allCommentsCol = commentsListSnapshot?.docs;
+  const [comments, commentsLoading, commentsError, commentsSnapshot] =
+    useCollectionData(commentsQuery, {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    });
 
-  const allComments = [];
-  allCommentsCol?.forEach((comment) => {
-    allComments.push(comment.data());
-  });
-  const tweetComments = allComments.filter((comment) => comment.tweetId === id);
-
-  console.log(tweet);
   return (
     <div className="px-[1.90rem] pb-[9.615rem]">
-      {tweet ? (
-        <>
-          <Tweet
-            tweetId={id ? id : ""}
-            likes={tweet.likes}
-            retweets={tweet.retweets}
-            media={tweet.media}
-            text={tweet.text}
-            timestamp={tweet.timestamp}
-            userId={tweet.userId}
-          />
+      {/* Rendering tweet states */}
+      {tweetError && <strong>Error: {JSON.stringify(tweetError)}</strong>}
+      {tweetLoading && <span>Collection: Loading...</span>}
+      {tweet && (
+        <Tweet
+          tweetId={id ? id : ""}
+          likes={tweet.likes}
+          retweets={tweet.retweets}
+          media={tweet.media}
+          text={tweet.text}
+          timestamp={tweet.timestamp}
+          userId={tweet.userId}
+        />
+      )}
 
-          <div>
-            <>
-              <p className="mb-4 text-[1.8rem] font-medium">Comments</p>
-              {tweetComments.length ? (
-                tweetComments.map((comment) => (
-                  <Comment
-                    text={comment.comment}
-                    timestamp={comment.timestamp}
-                    userId={comment.userId}
-                  />
-                ))
-              ) : (
-                <p>No comments to see</p>
-              )}
-            </>
-          </div>
-        </>
+      <p className="mb-4 text-[1.8rem] font-medium">Comments</p>
+      {/* Rendering comment states */}
+      {commentsError && <strong>Error: {JSON.stringify(commentsError)}</strong>}
+      {commentsLoading && <span>Collection: Loading...</span>}
+
+      {comments ? (
+        <div>
+          {comments.length ? (
+            comments.map((comment) => (
+              <Comment
+                text={comment.comment}
+                timestamp={comment.timestamp}
+                userId={comment.userId}
+              />
+            ))
+          ) : (
+            <p>No comments to see</p>
+          )}
+        </div>
       ) : (
-        "Loading"
+        "Loading comments"
       )}
     </div>
   );
