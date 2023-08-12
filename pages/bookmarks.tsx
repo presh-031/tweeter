a8aacc27c6e33300c92274292737fdccc77f795b
presh-031 <akingbulugbe.dev@gmail.com>
2023-08-12 14:56:29 -0700

    feat: added bookmarks, onto testing

--- pages/bookmarks.tsx
+++ pages/bookmarks.tsx
@@ -1,22 +1,76 @@
 import WithAuthUser from "@/components/WithAuthUser";
-import { db } from "@/config/firebase";
-import { collection, query, where } from "firebase/firestore";
-import { useCollection } from "react-firebase-hooks/firestore";
+import { auth, db } from "@/config/firebase";
+import { collection, doc, query, where } from "firebase/firestore";
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  useCollection,
+  useCollectionData,
+  useDocumentData,
+} from "react-firebase-hooks/firestore";
+import { Tweet } from "..";
 
 const Bookmarks = () => {
-  // const [currentUser, loading, error] = useAuthState(auth);
-  // const currentUserId = currentUser?.uid;
+  // Get current user info
+  const [currentUser, loadingCurrentUser, errorCurrentUser] =
+    useAuthState(auth);
+  const currentUserId = currentUser ? currentUser.uid : "";
+
+  const [userInfo, userInfoLoading, userInfoError] = useDocumentData(
+    doc(db, "users", currentUserId),
+    {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    }
+  );
+  const bookmarkedTweetsIds = userInfo?.bookmarkedTweets;
+
+  // Simply fetching all tweets and filtering on the FE.
+  const tweetsRef = collection(db, "tweets");
+  const [tweetSnapshots, loading, error] = useCollection(tweetsRef, {
+    snapshotListenOptions: { includeMetadataChanges: true },
+  });
+
+  const bookmarkedTweets = tweetSnapshots?.docs.filter((doc) =>
+    bookmarkedTweetsIds.includes(doc.id)
+  );
 
   // const tweetsRef = collection(db, "tweets");
 
-  // const tweetsQuery = query(tweetsRef, where("userId", "==", uid));
+  // let tweetsQuery;
+  // if (bookmarkedTweetsIds?.length) {
+  //   tweetsQuery = query(tweetsRef, where("tweetId", "in", bookmarkedTweetsIds));
+  //   console.log("bookmarks are there");
+  // } else {
+  //   // query that won't fetch anything
+  //   tweetsQuery = query(tweetsRef, where("tweetId", "==", ""));
+  //   console.log("bookmarks are not there");
+  // }
 
-  // const [tweetsListSnapshot, loading, error] = useCollection(tweetsQuery, {
+  // const [tweetSnapshots, loading, error] = useCollectionData(tweetsQuery, {
   //   snapshotListenOptions: { includeMetadataChanges: true },
   // });
-  // const userTweets = tweetsListSnapshot?.docs;
 
-  return <div className=" px-[1.90rem] pb-[9.615rem]">bookmarks</div>;
+  // console.log(tweetSnapshots);
+
+  return (
+    <div className=" px-[1.90rem] pb-[9.615rem]">
+      bookmarks
+      {bookmarkedTweets?.map((tweet) => {
+        return (
+          <Tweet
+            key={tweet.id}
+            tweetId={tweet.id}
+            likes={tweet.data()?.likes}
+            retweets={tweet.data()?.retweets}
+            media={tweet.data()?.media}
+            text={tweet.data()?.text}
+            timestamp={tweet.data()?.timestamp}
+            userId={tweet.data()?.userId}
+            bookmarkedBy={tweet.data()?.bookmarkedBy}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default WithAuthUser(Bookmarks);
