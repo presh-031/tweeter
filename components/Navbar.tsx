38c7aa24bb630d7493ca027044617e3dd306cb7f
presh-031 <akingbulugbe.dev@gmail.com>
2023-08-20 02:40:16 -0700

    feat: removed userinfo from nav

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,14 +20,14 @@ const Navbar = () => {
   };
 
   // Logic to get current user info for navbar
-  const [currentUser] = useAuthState(auth);
-  const currentUserId = currentUser ? currentUser.uid : "";
-  const [userInfo, userInfoLoading, userInfoError] = useDocumentData(
-    doc(db, "users", currentUserId),
-    {
-      snapshotListenOptions: { includeMetadataChanges: true },
-    }
-  );
+  // const [currentUser] = useAuthState(auth);
+  // const currentUserId = currentUser ? currentUser.uid : "";
+  // const [userInfo, userInfoLoading, userInfoError] = useDocumentData(
+  //   doc(db, "users", currentUserId),
+  //   {
+  //     snapshotListenOptions: { includeMetadataChanges: true },
+  //   }
+  // );
   // console.log(userInfo);
 
   return (
@@ -45,7 +45,7 @@ const Navbar = () => {
       </div>
 
       <div onClick={toggleShowUserNav} className="flex items-center gap-4 ">
-        <Image
+        {/* <Image
           src={
             userInfo?.profilePictureUrl
               ? userInfo.profilePictureUrl
@@ -61,7 +61,7 @@ const Navbar = () => {
           {userInfo?.displayName
             ? `@${userInfo.displayName}`
             : userInfo?.userName}
-        </p>
+        </p> */}
 
         {showUserNav ? (
           <RiArrowUpSFill className="text-3xl" />
