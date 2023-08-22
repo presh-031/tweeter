d24ac401832ad7ccc00f870c26106b36c89bd3b9
presh-031 <akingbulugbe.dev@gmail.com>
2023-08-22 00:54:39 -0700

    feat: added default values

--- components/EditProfileForm.tsx
+++ components/EditProfileForm.tsx
@@ -2,124 +2,106 @@ import React from "react";
 import * as yup from "yup";
 
 import { useRouter } from "next/router";
-import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { doc, updateDoc } from "firebase/firestore";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useState } from "react";
 import { auth, db } from "@/config/firebase";
 import { MdOutlineBrokenImage } from "react-icons/md";
-import userPlaceholder from "../assets/user-placeholder.png";
 import { useAuthState } from "react-firebase-hooks/auth";
 import toast from "react-hot-toast";
 import { useForm } from "react-hook-form";
 import { BiArrowBack } from "react-icons/bi";
 import Image from "next/image";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
 const schema = yup.object().shape({
-  userName: yup.string().required("You must add a name"),
-  email: yup.string().required("You must add an email"),
-  displayName: yup.string().required("You must add a displayName"),
-  profilePictureUrl: yup.string().required("You must add a profile picture"),
-  headerImageUrl: yup.string().required("You must add a header image"),
-  bio: yup.string(),
+  userName: yup.string().required("Please input a User Name."),
+  displayName: yup.string().required("Please input a Display Name."),
+  bio: yup.string().required("Please add something about you."),
 });
 type FormData = yup.InferType<typeof schema>;
 
 const EditProfileForm = () => {
   const router = useRouter();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
 
-  // const [currentUser] = useAuthState(auth);
-  // const currentUserId = currentUser ? currentUser.uid : "";
-  // const [loading, setLoading] = useState<boolean>(false);
+  const [authUser] = useAuthState(auth);
+  const authUserId = authUser ? authUser.uid : "";
+  const [authUserInfo, authUserInfoLoading, authUserInfoError] =
+    useDocumentData(doc(db, "users", authUserId), {
+      snapshotListenOptions: { includeMetadataChanges: true },
+    });
 
-  // const {
-  //   register,
-  //   handleSubmit,
-  //   formState: { errors },
-  // } = useForm<FormData>({
-  //   resolver: yupResolver(schema),
-  // });
+  // const [loading, setLoading] = useState(false);
+  console.log(authUserInfo);
 
-  // const onSubmit = async (data: FormData) => {
-  //   setLoading(true);
-  //   const userDocRef = doc(db, "users", currentUserId);
-  //   try {
-  //     await updateDoc(userDocRef, {
-  //       userName: data.userName,
-  //       email: data.email,
-  //       displayName: data.displayName,
-  //       profilePictureUrl: data.profilePictureUrl,
-  //       headerImageUrl: data.headerImageUrl,
-  //       bio: data.bio,
-  //     });
-  //     setLoading(false);
-  //     toast.success("Successfully edited.");
-  //     router.push("/");
-  //   } catch (err) {
-  //     toast.error("Try again.");
-  //     router.push("/");
-  //     alert(err);
-  //   }
-  // };
+  const onSubmit = async (data: FormData) => {
+    console.log(data);
+    //   setLoading(true);
+    //   const userDocRef = doc(db, "users", currentUserId);
+    //   try {
+    //     await updateDoc(userDocRef, {
+    //       userName: data.userName,
+    //       email: data.email,
+    //       displayName: data.displayName,
+    //       profilePictureUrl: data.profilePictureUrl,
+    //       headerImageUrl: data.headerImageUrl,
+    //       bio: data.bio,
+    //     });
+    //     setLoading(false);
+    //     toast.success("Successfully edited.");
+    //     router.push("/");
+    //   } catch (err) {
+    //     toast.error("Try again.");
+    //     router.push("/");
+    //     alert(err);
+    //   }
+  };
+
   return (
     <>
-      <div>
-        <div className="relative h-[8rem] w-full rounded-[.8rem] bg-[#777777] bg-opacity-80">
-          <div className="absolute top-[1rem] left-[1rem] w-fit rounded-full bg-black bg-opacity-50 p-2">
-            <MdOutlineBrokenImage className=" text-3xl text-white" />
-          </div>
-        </div>
-        <div className="relative mt-8 h-[5rem] w-[5rem] overflow-hidden rounded-[.8rem] bg-black bg-opacity-20">
-          <Image
-            src={userPlaceholder}
-            alt="select-profile-pic"
-            width={50}
-            height={50}
-            className="opacity-30 "
-          />
-          <div className="absolute top-1/2 left-1/2 w-fit -translate-x-1/2 -translate-y-1/2 transform rounded-full bg-black bg-opacity-60 p-2">
-            <MdOutlineBrokenImage className=" text-3xl text-white" />
-          </div>
-        </div>
-      </div>
-      <form
-        className="  mt-10 text-2xl"
-        // onSubmit={handleSubmit(onSubmit)}
-      >
+      <form className="  mt-10 text-2xl" onSubmit={handleSubmit(onSubmit)}>
         <label className="mb-4 text-[#333333] " htmlFor="name">
           User Name
         </label>
         <input
-          // {...register("userName")}
+          {...(register("userName"), { defaultValue: authUserInfo?.userName })}
           className="mb-8 block w-full rounded-xl border-[1px] border-[#333333] bg-transparent p-4 outline-none"
           type="text"
           id="userName"
         />
-        {/* <p style={{ color: "red" }}>{errors.userName?.message}</p> */}
+        <p style={{ color: "red" }}>{errors.userName?.message}</p>
 
         <label className="mb-4  text-[#333333]" htmlFor="displayName">
           Display Name
         </label>
         <input
-          // {...register("displayName")}
+          {...(register("displayName"),
+          { defaultValue: authUserInfo?.displayName })}
           className="mb-8 block w-full rounded-xl border-[1px] border-[#333333] bg-transparent p-4 outline-none"
           type="text"
           id="displayName"
         />
-        {/* <p style={{ color: "red" }}>{errors.displayName?.message}</p> */}
+        <p style={{ color: "red" }}>{errors.displayName?.message}</p>
 
         <label className="mb-4  text-[#333333]" htmlFor="bio">
           Bio
         </label>
         <textarea
-          // {...register("bio")}
+          {...(register("bio"), { defaultValue: authUserInfo?.bio })}
           className="mb-8 block w-full rounded-xl border-[1px] border-[#333333] bg-transparent p-4 outline-none"
           name="bio"
           id="bio"
           cols={20}
           rows={5}
         ></textarea>
-        {/* <p style={{ color: "red" }}>{errors.bio?.message}</p>  */}
+        <p style={{ color: "red" }}>{errors.bio?.message}</p>
 
         <div className="flex items-center justify-between gap-4">
           <button
