c1ebf9b35d1baa5f3452b4e73e9f3e3d2187d320
presh-031 <akingbulugbe.dev@gmail.com>
2023-08-27 04:30:24 -0700

    feat: added login page, onto reviewing authflow

---
+++ components/LoginForm.tsx
@@ -0,0 +1,102 @@
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import {
+  useCreateUserWithEmailAndPassword,
+  useSignInWithEmailAndPassword,
+} from "react-firebase-hooks/auth";
+import { auth } from "@/config/firebase";
+import { MdEmail } from "react-icons/md";
+import { IoMdLock } from "react-icons/io";
+import { createNewUserInDb } from "@/helpers/authHelpers";
+import { useRouter } from "next/router";
+import SignInLoader from "./Loaders/SignInLoader";
+import { useState } from "react";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .email("Invalid email format")
+    .required("Email is required"),
+  password: yup
+    .string()
+    .min(8, "Password must be at least 8 characters long")
+    .required("Password is required"),
+  fullName: yup.string().required("Full name is required"),
+});
+type FormData = yup.InferType<typeof schema>;
+
+const LogInForm = () => {
+  const router = useRouter();
+  const [createUserWithEmailAndPassword, user, loading, error] =
+    useCreateUserWithEmailAndPassword(auth);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const [fullName, setFullName] = useState("");
+  const onSubmit = (data: FormData) => {
+    createUserWithEmailAndPassword(data.email, data.password);
+    setFullName(data.fullName);
+  };
+
+  if (user?.user) {
+    createNewUserInDb(user, fullName);
+    router.push("/");
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-6">
+      <div>
+        <div className="flex items-center gap-4 overflow-hidden rounded-xl  bg-gray-100  pl-4">
+          <MdEmail />
+          <input
+            type="text"
+            {...register("email")}
+            placeholder="Email"
+            className="w-full bg-transparent py-4 text-2xl outline-none"
+          />
+        </div>
+        <p className="text-red-600">{errors.email?.message}</p>
+        {error && <p className="text-red-600">Email already being used</p>}
+      </div>
+
+      <div>
+        <div className="flex items-center gap-4 overflow-hidden rounded-xl  bg-gray-100 pl-4">
+          <IoMdLock />
+          <input
+            type="password"
+            {...register("password")}
+            placeholder="Password"
+            className="w-full bg-transparent py-4 text-2xl outline-none"
+          />
+        </div>
+        <p className="text-red-600">{errors.password?.message}</p>
+      </div>
+
+      <div className="relative flex items-center">
+        <input
+          type="submit"
+          disabled={loading}
+          value="Start tweeting now"
+          className={`${
+            loading ? "bg-opacity-30" : ""
+          } mt-2 w-full rounded-xl border bg-blueish py-4 text-2xl font-medium text-white`}
+        />
+
+        {loading && (
+          <div className="absolute right-10">
+            <SignInLoader />
+          </div>
+        )}
+      </div>
+    </form>
+  );
+};
+
+export default LogInForm;
--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -1,5 +1,34 @@
+import LoginForm from "@/components/LoginForm";
+import { GoogleBtn, SignInForm } from "..";
+import logo from "../assets/tweeter.svg";
+import Image from "next/image";
+import { IconContext } from "react-icons";
+import GuestBtn from "@/components/ui/GuestBtn";
+import Link from "next/link";
+
 const login = () => {
-  return <div>This page is a work in progress.</div>;
+  return (
+    <IconContext.Provider value={{ className: "react-icons sign-in-icons" }}>
+      <div className=" bg-white py-24 px-20">
+        <Image src={logo} alt="logo" />
+        <p className="mb-12 mt-8 text-2xl font-semibold"></p>
+
+        <LoginForm />
+        <p className="my-6 text-center text-xl">or</p>
+        <GoogleBtn />
+
+        {/* add functionality to guest btn */}
+        <GuestBtn />
+
+        <p className="mt-12 text-center text-xl">
+          Not a member?{" "}
+          <Link href="/sign-in" className="text-blueish">
+            Sign up
+          </Link>
+        </p>
+      </div>
+    </IconContext.Provider>
+  );
 };
 
 export default login;
