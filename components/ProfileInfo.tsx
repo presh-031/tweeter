30b959999915d7b86ca775b6dd98b46fac02f69d
presh-031 <akingbulugbe.dev@gmail.com>
2023-09-03 14:41:03 -0700

    feat: finished making profile page responsive

--- components/ProfileInfo.tsx
+++ components/ProfileInfo.tsx
@@ -9,7 +9,7 @@ const ProfileInfo = ({
   return (
     <div className="">
       <div>
-        <h1 className="text-[2.4rem] font-semibold leading-[3.6rem] tracking-[-3.5%] text-[#333333]">
+        <h1 className="mt-[2.5rem] text-[2.4rem] font-semibold leading-[3.6rem] tracking-[-3.5%] text-[#333333]">
           {userName}
         </h1>
         <div className="mt-[.4rem] mb-[1.4rem] flex items-center justify-center gap-8 text-[1.2rem] font-medium leading-[1.8rem] tracking-[-3.5%] text-[#828282] ">
