18c32c8e1905797ab0baa5be78e1576fa4b69da5
presh-031 <akingbulugbe.dev@gmail.com>
2023-09-10 19:26:26 -0700

    chore:testing

--- hooks/useImageUploader.tsx
+++ hooks/useImageUploader.tsx
@@ -4,7 +4,7 @@ const useImageUploader = () => {
     imageRef: any,
     uploadFile: any,
     saveCoverImageMetaData: any,
-    newTweetId: any
+    newTweetId?: any
   ) => {
     if (selectedImage) {
       const result = await uploadFile(imageRef, selectedImage, {
@@ -13,7 +13,13 @@ const useImageUploader = () => {
 
       if (result?.metadata) {
         const fullPath = result.metadata.fullPath;
-        saveCoverImageMetaData(fullPath, newTweetId);
+
+        // newTweetId might be falsy as profilepic and cover img uploads use the same hook.
+        if (newTweetId) {
+          saveCoverImageMetaData(fullPath, newTweetId);
+        } else {
+          saveCoverImageMetaData(fullPath);
+        }
       }
     }
   };
